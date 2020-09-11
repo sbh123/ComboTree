3dca3a4ab13184b08a0f3d66037f4cdc9ffac551
张丘洋 <cs.qyzhang@qq.com>
2020-09-11 15:47:52 +0800

    添加了Clevel与LeafNode的测试

--- tests/clevel_vs_leaf.cc
+++ tests/clevel_vs_leaf.cc
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <filesystem>
+#include <mutex>
 #include "clevel.h"
 #include "slab.h"
 #include "random.h"
 
 using combotree::CLevel;
+using combotree::CLevel::Mutex;
 using combotree::Status;
 using combotree::Slab;
+using combotree::CLevel::LeafNode;
 using namespace std;
 
-#define PATH        "/mnt/pmem0/persistent"
+#define PATH      "/mnt/pmem0/persistent"
 #define TREE_NUM  100000
-#define TREE_SIZE 150
+#define TREE_SIZE 3
 
 int main(void) {
   std::filesystem::remove(PATH);
@@ -21,10 +24,16 @@ int main(void) {
   CLevel::MemoryManagement mem(pop, slab);
 
   CLevel* db[TREE_NUM];
+  LeafNode* nodes[TREE_NUM];
+  Mutex mutex[TREE_NUM]
 
   for (int i = 0; i < TREE_NUM; ++i) {
-    db[i] = new combotree::CLevel();
+    db[i] = new CLevel();
     db[i]->InitLeaf(&mem);
+    db[i]->mutex_.GetLeafMutex(0);
+    nodes[i] = mem.NewLeafNode();
+    nodes[i].id = 0;
+    mutex[i].GetLeafMutex(0);
   }
 
   combotree::RandomUniformUint64 rnd;
@@ -34,14 +43,22 @@ int main(void) {
   }
 
   combotree::Timer timer;
+
   timer.Start();
   for (int i = 0; i < TREE_NUM; ++i) {
     for (int j = 0; j < TREE_SIZE; ++j) {
       db[i]->Insert(&mem, key[j], key[j]);
     }
   }
+  std::cout << "CLevel elapsed time: " << timer.End() << std::endl;
 
-  std::cout << "Elapsed time: " << timer.End() << std::endl;
+  timer.Start();
+  for (int i = 0; i < TREE_NUM; ++i) {
+    for (int j = 0; j < TREE_SIZE; ++j) {
+      nodes[i]->Insert(&mem, mutex[i], key[j], key[j]);
+    }
+  }
+  std::cout << "LeafNode elapsed time: " << timer.End() << std::endl;
 
   return 0;
 }
