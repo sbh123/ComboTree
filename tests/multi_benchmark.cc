982d287d12a915bda4f44d31befa9eb9a1a989fd
张丘洋 <cs.qyzhang@qq.com>
2020-11-11 14:08:18 +0800

    add multi_benchmark

--- tests/multi_benchmark.cc
+++ tests/multi_benchmark.cc
@@ -3,18 +3,20 @@
 #include <cassert>
 #include <iomanip>
 #include <sstream>
+#include <thread>
 #include "combotree/combotree.h"
 #include "combotree_config.h"
 #include "random.h"
 #include "timer.h"
 
-#define TEST_SIZE       10000000
-#define LAST_EXPAND     600000
-#define GET_SIZE        1000000
-#define SCAN_TEST_SIZE  1000000
+size_t TEST_SIZE      = 10000000;
+size_t LAST_EXPAND    = 6000000;
+size_t GET_SIZE       = 1000000;
+size_t SCAN_TEST_SIZE = 1000000;
+size_t SCAN_SIZE      = 100;
 
-bool use_data_file = true;
-int SCAN_SIZE = 100;
+int thread_num      = 4;
+bool use_data_file  = true;
 
 using combotree::ComboTree;
 using combotree::Random;
@@ -51,9 +53,18 @@ int main(int argc, char** argv) {
   ComboTree* tree = new ComboTree("/mnt/pmem0/", (1024*1024*512UL), true);
 #endif
 
-  if (argc == 2) {
+  if (argc >= 2) {
     SCAN_SIZE = atoi(argv[1]);
   }
+
+  if (argc >= 3) {
+    thread_num = atoi(argv[2]);
+  }
+
+  TEST_SIZE      = (TEST_SIZE / thread_num) * thread_num;
+  LAST_EXPAND    = (LAST_EXPAND / thread_num) * thread_num;
+  GET_SIZE       = (GET_SIZE / thread_num) * thread_num;
+  SCAN_TEST_SIZE = (SCAN_TEST_SIZE / thread_num) * thread_num;
 
   std::cout << "TEST_SIZE:             " << TEST_SIZE << std::endl;
   std::cout << "LAST_EXPAND:           " << LAST_EXPAND << std::endl;
@@ -74,8 +85,7 @@ int main(int argc, char** argv) {
 
   if (use_data_file) {
     std::ifstream data("./data.dat");
-
-    for (int i = 0; i < TEST_SIZE; ++i) {
+    for (size_t i = 0; i < TEST_SIZE; ++i) {
       uint64_t k;
       data >> k;
       key.push_back(k);
@@ -83,28 +93,50 @@ int main(int argc, char** argv) {
     }
   } else {
     Random rnd(0, TEST_SIZE-1);
-    for (int i = 0; i < TEST_SIZE; ++i)
+    for (size_t i = 0; i < TEST_SIZE; ++i)
       key.push_back(i);
-    for (int i = 0; i < TEST_SIZE; ++i)
+    for (size_t i = 0; i < TEST_SIZE; ++i)
       std::swap(key[i],key[rnd.Next()]);
   }
 
   uint64_t value;
   Timer timer;
 
-  // Put
+  std::vector<std::thread> threads;
+  size_t per_thread_size;
+
+  // LOAD
+  per_thread_size = LAST_EXPAND / thread_num;
   timer.Record("start");
-  for (size_t i = 0; i < LAST_EXPAND; ++i)
-    assert(tree->Put(key[i], key[i]) == true);
+  for (int i = 0; i < thread_num; ++i) {
+    threads.emplace_back([=,&key](){
+      size_t start_pos = i*per_thread_size;
+      for (size_t j = 0; j < per_thread_size; ++j)
+        assert(tree->Put(key[start_pos+j], key[start_pos+j]) == true);
+    });
+  }
+  for (auto& t : threads)
+    t.join();
+  threads.clear();
+
   timer.Record("mid");
-  for (size_t i = LAST_EXPAND; i < TEST_SIZE; ++i)
-    assert(tree->Put(key[i], key[i]) == true);
+  per_thread_size = (TEST_SIZE - LAST_EXPAND) / thread_num;
+  for (int i = 0; i < thread_num; ++i) {
+    threads.emplace_back([=,&key](){
+      size_t start_pos = i*per_thread_size+LAST_EXPAND;
+      for (size_t j = 0; j < per_thread_size; ++j)
+        assert(tree->Put(key[start_pos+j], key[start_pos+j]) == true);
+    });
+  }
+  for (auto& t : threads)
+    t.join();
+  threads.clear();
   timer.Record("stop");
 
   uint64_t total_time = timer.Microsecond("mid", "start");
-  std::cout << "load: " << total_time/1000000.0 << " " << (double)TEST_SIZE/total_time*1000000.0 << std::endl;
+  std::cout << "load: " << total_time/1000000.0 << " " << (double)TEST_SIZE/(double)total_time*1000000.0 << std::endl;
   uint64_t mid_time = timer.Microsecond("stop", "mid");
-  std::cout << "put:  " << mid_time/1000000.0 << " " << (double)(TEST_SIZE-LAST_EXPAND)/mid_time*1000000.0 << std::endl;
+  std::cout << "put:  " << mid_time/1000000.0 << " " << (double)(TEST_SIZE-LAST_EXPAND)/(double)mid_time*1000000.0 << std::endl;
 
   std::cout << "clevel time:    " << tree->CLevelTime()/1000000.0 << std::endl;
 
@@ -119,34 +151,52 @@ int main(int argc, char** argv) {
   tree->BLevelCompression();
 
   // Get
+  per_thread_size = GET_SIZE / thread_num;
   timer.Clear();
   timer.Record("start");
-  for (int i = 0; i < GET_SIZE; ++i) {
-    uint64_t target = key[i];
-    assert(tree->Get(target, value) == true);
-    assert(value == target);
+  for (int i = 0; i < thread_num; ++i) {
+    threads.emplace_back([=,&key](){
+      size_t start_pos = i*per_thread_size;
+      size_t value;
+      for (size_t j = 0; j < per_thread_size; ++j) {
+        assert(tree->Get(key[start_pos+j], value) == true);
+        assert(value == key[start_pos+j]);
+      }
+    });
   }
+  for (auto& t : threads)
+    t.join();
+  threads.clear();
   timer.Record("stop");
   total_time = timer.Microsecond("stop", "start");
   std::cout << "get: " << total_time/1000000.0 << " " << (double)GET_SIZE/(double)total_time*1000000.0 << std::endl;
 
-  for (uint64_t i = TEST_SIZE; i < TEST_SIZE+10000; ++i) {
+  for (size_t i = TEST_SIZE; i < TEST_SIZE+10000; ++i) {
     assert(tree->Get(i, value) == false);
   }
 
   // scan
   timer.Clear();
+  per_thread_size = SCAN_TEST_SIZE / thread_num;
   timer.Record("start");
-  for (int i = 0; i < SCAN_TEST_SIZE; ++i) {
-    uint64_t start_key = key[i];
-    ComboTree::Iter iter(tree, start_key);
-    for (int j = 0; j < SCAN_SIZE; ++j) {
-      assert(iter.key() == start_key + j);
-      assert(iter.value() == start_key + j);
-      if (!iter.next())
-        break;
-    }
+  for (int i = 0; i < thread_num; ++i) {
+    threads.emplace_back([=,&key](){
+      size_t start_pos = i*per_thread_size;
+      for (size_t j = 0; j < per_thread_size; ++j) {
+        uint64_t start_key = key[start_pos+j];
+        ComboTree::Iter iter(tree, start_key);
+        for (size_t k = 0; k < SCAN_SIZE; ++k) {
+          assert(iter.key() == start_key + k);
+          assert(iter.value() == start_key + k);
+          if (!iter.next())
+            break;
+        }
+      }
+    });
   }
+  for (auto& t : threads)
+    t.join();
+  threads.clear();
   timer.Record("stop");
   total_time = timer.Microsecond("stop", "start");
   std::cout << "scan " << SCAN_SIZE << ": " << total_time/1000000.0 << " " << (double)SCAN_TEST_SIZE/(double)total_time*1000000.0 << std::endl;
